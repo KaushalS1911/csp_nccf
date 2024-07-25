72b5acfc46f296717d0ebad5f753ab48bc886d1e
KaushalS19 <kaushalsojitra19@gmail.com>
2024-07-25 11:29:38 +0530

    miller + distributor UI changes

---
+++ dist/assets/upload-document-PApTxiyy.js
@@ -0,0 +1,1 @@
+import{j as t,cD as s}from"./index-YS5soo1u.js";const r=()=>t.jsx(t.Fragment,{children:t.jsx(s,{})});export{r as default};
--- dist/assets/upload-document-eUvVEyFe.js
+++
@@ -1,1 +0,0 @@
-import{j as t,cD as s}from"./index-iex044-7.js";const r=()=>t.jsx(t.Fragment,{children:t.jsx(s,{})});export{r as default};
